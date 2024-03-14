182da2a15227379673cdf977e743df423a0621c0
Henievich <henievich@gmail.com>
2024-03-14 19:24:35 +0300

    commit

--- lesson_1/project_1/app_1/views.py
+++ lesson_1/project_1/app_1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def url1(request):
+    return HttpResponse("Hello, world. You're at the Earth")
+
+
+def url2(request):
+    return HttpResponse("Hello, world. You're at the moon")
