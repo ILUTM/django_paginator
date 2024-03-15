4b7395ce7bb52064713389ec4ca493c3eee2f67a
Henievich <henievich@gmail.com>
2024-03-16 00:37:21 +0300

    commit

--- lesson_2_parser/django_parser/django_parser/urls.py
+++ lesson_2_parser/django_parser/django_parser/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('kufar/', include('kufar.urls')),
     path('admin/', admin.site.urls),
+    path('kufar/', include('kufar.urls')),
 ]
 
--- lesson_2_parser/django_parser/kufar/urls.py
+++ lesson_2_parser/django_parser/kufar/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('1', views.url1),
-    path('2', views.url2),
+    path('1/', views.url1),
+    path('2/', views.url2),
 ]
